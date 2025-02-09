4fc3c55ef8c4a91eb294f26734c3f5d2daaddf1a
muhammadibrahim <mik237@gmail.com>
2025-02-09 15:34:00 +0500

    SplashPage created

---
+++ composeApp/src/commonMain/kotlin/me/ibrahim/ebank/kmp/presentation/ui/SplashPage.kt
@@ -0,0 +1,9 @@
+package me.ibrahim.ebank.kmp.presentation.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SplashPage(modifier: Modifier = Modifier) {
+
+}
