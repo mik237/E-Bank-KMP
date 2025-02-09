f2ea2fb6b8f9deced7e1c5eed7987fb280a51ba2
muhammadibrahim <mik237@gmail.com>
2025-02-09 15:35:46 +0500

    SplashPage updated

--- composeApp/src/commonMain/kotlin/me/ibrahim/ebank/kmp/presentation/ui/SplashPage.kt
+++ composeApp/src/commonMain/kotlin/me/ibrahim/ebank/kmp/presentation/ui/SplashPage.kt
@@ -1,9 +1,11 @@
 package me.ibrahim.ebank.kmp.presentation.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun SplashPage(modifier: Modifier = Modifier) {
-
+fun SplashPage() {
+    Box(modifier = Modifier.fillMaxSize())
 }
